8ee4128917b56f99775708fb77005d3c996181ce
philippe desjardins <phil_94@mac.com>
2019-08-08 15:08:08 -0400

    started seeding consultant and services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Consultant.create([{ name: 'Stéphane Guérard', bio: 'lorem ipsum', photo: 'url', expertise: 'consultant'}])
+Consultant.create([{ name: 'Éric Steedman', bio: 'lorem ipsum', photo: 'url', expertise: 'consultant'}])
+Consultant.create([{ name: 'Javier M Vaquero', bio: 'lorem ipsum', photo: 'url', expertise: 'consultant'}])	
+
+Service.create([{ 
+	name: 'Études, analyses et recherche', 
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'}])
+Service.create([{ 
+	name: 'Stratégies d’organisation', 
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'}])
+Service.create([{ 
+	name: 'Soutien à la gestion humaine', 
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'}])
+Service.create([{ 
+	name: 'Animation et formation', 
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'}])
+Service.create([{ 
+	name: 'Évaluation', 
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'}])
+Service.create([{ 
+	name: 'Divers', 
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing.'}])
+
+
+puts "seed is complete!"
