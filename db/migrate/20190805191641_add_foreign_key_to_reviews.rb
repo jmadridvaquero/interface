6752a2976a4455d75c65e060401ee1f220f159e6
Javier Madrid <jmadridvaquero@gmail.com>
2019-08-09 10:40:26 -0400

    bug fixed

--- db/migrate/20190805191641_add_foreign_key_to_reviews.rb
+++ db/migrate/20190805191641_add_foreign_key_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToReviews < ActiveRecord::Migration[5.2]
   def change
-  	add_reference :reviews, :services, index: true, foreign_key: true
+  	add_reference :reviews, :service, index: true, foreign_key: true
   end
 end
