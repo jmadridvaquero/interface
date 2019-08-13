12ff6d37dd5c331d890fbf3dadd39cb7ea038b28
philippe desjardins <phil_94@mac.com>
2019-08-13 11:03:49 -0400

    added rails contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  
+  root to: 'pages#home'
+
   resources :consultants, only: [:index, :show]
   resources :reviews, only: [:index, :show]
   resources :services, only: [:index, :show]
+  resources :contact, only: [:index, :new, :create]
 
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get '/about',   to: 'pages#about'
-  	get '/contact', to: 'pages#contact'
+  	get 'contact', to: 'contact#index'
+
 end
