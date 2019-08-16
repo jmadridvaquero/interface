95271a3153741318e47e7fec47443d6feb824f71
Javier Madrid <jmadridvaquero@gmail.com>
2019-08-16 14:37:09 -0400

    switching languages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  scope "(:locale)", locale: /fr|en/ do
+  	resources :home
+  	resources :consultants
+  	resources :reviews
+  	resources :services
+  	resources :contact
+	end
+
   resources :consultants, only: [:index, :show]
   resources :reviews, only: [:index, :show]
   resources :services, only: [:index, :show]
@@ -8,6 +16,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get '/about',   to: 'pages#about'
-  	get 'contact', to: 'contact#index'
+  	get '/contact', to: 'contact#index'
+  	get '/:locale', to: 'pages#home'
+  	get '/:locale', to: 'pages#about'
+  	get '/:locale', to: 'contact#index'
 
 end
