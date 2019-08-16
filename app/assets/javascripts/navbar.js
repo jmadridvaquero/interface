88dc7e529630806a72d7178029b3156633ff99ee
philippe desjardins <phil_94@mac.com>
2019-08-16 17:06:49 -0400

    fixed navbar issue

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,18 @@
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 650) {
-        $('.secondary_navbar-coop').fadeIn();
-    } else {
-        $('.secondary_navbar-coop').fadeOut();
-    }
+function scrollHandler() {
+  var y = $(window).scrollTop();
+  var $navBar = $('.secondary_navbar-coop');
 
+  if (y > 650) {
+    if (!$navBar.is(':visible')) {
+      $navBar.css({opacity: 0, display: 'flex'}).animate({opacity: 1}, 400);
+    }
+  } else {
+    $navBar.fadeOut();
+  }
+}
+
+$(function() {
+  $(document).scroll(scrollHandler);
+  // scrollHandler will be called when the dom is ready
+  scrollHandler();
 });
